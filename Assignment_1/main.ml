8d1d39c380b99ee5837dc8b826bc4ac69feb5e0b
Rasmus Haversen <rhaversen@gmail.com>
2024-09-04 14:46:56 +0000

    Add printing of new lines

--- Assignment_1/main.ml
+++ Assignment_1/main.ml
@@ -5,9 +5,11 @@ open Asm_example
 let () =
   Printf.printf "Running tests from Eval module:\n";
   Eval.test ();
+  Printf.printf "\n";
 
   Printf.printf "Running tests from PrettyPrinter module:\n";
   PrettyPrinter.test ();
+  Printf.printf "\n";
 
   Printf.printf "Printing the example assembly code:\n";
   Asm_example.printExample ()
